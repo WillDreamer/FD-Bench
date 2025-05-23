ae3c5b106a34e91524d1de7d681882788bb8d070
Haixin Wang <whx@cs.ucla.edu>
2025-05-23 02:04:15 -0700

    Update evaluate.sh

--- src/evaluate.sh
+++ src/evaluate.sh
@@ -5,7 +5,7 @@ EXP_NAME="PDE_CNS_fourier_next_step_variable_lambdalr_Rollout_20_0519-18:09"
 RESUME_STEP=138000
 
 
-CONFIG_PATH="/wanghaixin/FD-Bench/config/test/${TARGET}/${SPATIAL_REP}+${TEMPORAL_REP}.yaml"
+CONFIG_PATH="your_path/FD-Bench/config/test/${TARGET}/${SPATIAL_REP}+${TEMPORAL_REP}.yaml"
 
 cd /wanghaixin/FD-Bench
 /root/anaconda3/bin/accelerate launch --main_process_port 29513 src/evaluate.py \
